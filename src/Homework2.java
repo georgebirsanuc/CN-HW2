eca367efcef3fd23c2f247a9a0c2464e4c937ea3
Birsanuc George-Cristian <Birsanuc George-Cristian>
2017-03-16 21:07:15 +0200

    in progress

--- src/Homework2.java
+++ src/Homework2.java
@@ -3,46 +3,71 @@ public class Homework2 {
 
 	public static void main(String[] args) {
 		int n = 3;
-		double[][] x = { { 2, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-		System.out.println(calcDeterminant(x, 3));
-		System.out.println(verifyInputMatrix(x, n));
+		double[] d = new double[n];
+		double[][] A = { { 1, -1, 2 }, { -1, 5, -4 }, { 2, -4, 6 } };
+		// { 1, -1, 2 }
+		// { -1, 5, -4 }
+		// { 2, -4, 6 }
+		boolean matrixIsValid = isMatrixValid(A, n);
+
+		System.out.println("Det(A) = " + calcDeterminant(A, 3));
+		System.out.println("Matrice valida: " + matrixIsValid);
+
+		if (matrixIsValid) {
+			// D
+			for (int i = 0; i < n; ++i) {
+				d[i] = A[i][i];
+				for (int j = 0; j <= i - 1; ++j)
+					d[i] -= (A[i][i]/A[i][i])*d[j];
+				
+			}
+			System.out.print("D = ");
+			for (double it : d)
+				System.out.print(it + " ");
+			System.out.println();
+		}
 	}
 
-	public static boolean verifyInputMatrix(double[][] a, int n) {
+	public static boolean isMatrixValid(double[][] a, int n) {
 		if (a.length != a[0].length)
-			return false;
+			throw new RuntimeException("Matricea nu este patratica!");
 
-		for (int i = 1; i < a.length / 2; ++i)
-			for (int j = 1; j < a.length / 2; ++j) {
+		for (int i = 1; i < n; ++i)
+			for (int j = 1; j < n; ++j) {
 				if (a[i][j] != a[j][i])
-					return false;
+					throw new RuntimeException("Matricea nu este simetrica!");
 			}
-		for (int k = 1; k <= n; ++k)
-			for (int i = 0; i < n - k + 1; ++i)
-				for (int j = 0; j < n - k + 1; ++j) {
-					double aux[][] = getSubMatrix(a, k, i, j);
-					if (calcDeterminant(aux, aux.length) == 0)
-						return false;
-				}
+
+		// for (int k = 1; k <= n; ++k)
+		// for (int i = 0; i < n - k + 1; ++i)
+		// for (int j = 0; j < n - k + 1; ++j) {
+		// double aux[][] = getSubMatrix(a, k, i, j);
+		// if (calcDeterminant(aux, aux.length) == 0)
+		// throw new RuntimeException("Matricea nu este pozitiv definita!");
+		// }
+
+		if (calcDeterminant(a, n) == 0)
+			return false;
 
 		return true;
 	}
 
-	public static double[][] getSubMatrix(double[][] a, int n, int posI, int posJ) {
-		double[][] r = new double[n][n];
-
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				r[i][j] = a[i + posI][j + posJ];
-				System.out.print(r[i][j] + " ");
-			}
-			System.out.println();
-			if (i == n - 1)
-				System.out.println();
-		}
-
-		return r;
-	}
+	// public static double[][] getSubMatrix(double[][] a, int n, int posI, int
+	// posJ) {
+	// double[][] r = new double[n][n];
+	//
+	// for (int i = 0; i < n; ++i) {
+	// for (int j = 0; j < n; ++j) {
+	// r[i][j] = a[i + posI][j + posJ];
+	// // System.out.print(r[i][j] + " ");
+	// }
+	// // System.out.println();
+	// // if (i == n - 1)
+	// // System.out.println();
+	// }
+	//
+	// return r;
+	// }
 
 	public static double calcDeterminant(double[][] a, int n) {
 		double r = 0;
